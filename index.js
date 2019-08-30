285c7466aee1a00693193f90dfcda83c7d67863c
Alexander Ozornin <alexozornin97@gmail.com>
2019-08-30 16:21:32 +0500

    v 0.1.0b

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+    afs: require('./lib/afs.js'),
+    array: require('./lib/array.js'),
+    child: require('./lib/child.js'),
+    chrono: require('./lib/chrono.js'),
+    crypto: require('./lib/crypto.js'),
+    function: require('./lib/function.js'),
+    math: require('./lib/math.js'),
+    object: require('./lib/object.js'),
+    path: require('./lib/path.js')
+}
